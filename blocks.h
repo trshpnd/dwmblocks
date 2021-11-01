31f966e9a8ad9bd17732a16ca4fecf2569f3871b
trshpnd <brcts990@gmail.com>
2021-11-01 15:46:20 -0300

    Directory changes

--- blocks.h
+++ blocks.h
@@ -4,10 +4,10 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	//{"/\x02", 	"sh /home/trshpnd/dwmblocks/scripts/network.sh", 	10, 	0},
 	//{"\x05  ",	"sh /home/trshpnd/dwmblocks/scripts/volume.sh", 	-1, 	10},
-	{"/\x02", 	"sh /home/trshpnd/dwmblocks/scripts/weather.sh", 		3600, 	0},
-	{"\x07", 	"sh /home/trshpnd/dwmblocks/scripts/date.sh", 			10, 	0},
-	{"\x05",	"sh /home/trshpnd/dwmblocks/scripts/time.sh",			10, 	0},
-	{"\x01",    "sh /home/trshpnd/dwmblocks/scripts/battery.sh",        60,     0},
+	{"/\x02", 	"sh /home/trshpnd/Programas/dwmblocks/scripts/weather.sh", 		3600, 	0},
+	{"\x07", 	"sh /home/trshpnd/Programas/dwmblocks/scripts/date.sh", 			10, 	0},
+	{"\x05",	"sh /home/trshpnd/Programas/dwmblocks/scripts/time.sh",			10, 	0},
+	{"\x01",    "sh /home/trshpnd/Programas/dwmblocks/scripts/battery.sh",        60,     0},
 };
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = "/";
