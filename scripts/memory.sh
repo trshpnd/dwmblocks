58b698dd8e37c97bf7668a118ce8dc716303b474
trshpnd <brcts990@gmail.com>
2021-11-30 15:39:07 -0300

    bug fix

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-mem_str=$(free -m | grep Mem | awk '{print $3}')
+mem_str=$(free -h | grep Mem | awk '{print $3}')
 #'/$2 * 100.0}')
 mem_icon=
+printf "%s %s" "$mem_icon" "$mem_str"
 
-if [ "$mem_str" -ge 1024 ]
-then
-	let mem_str=mem_str/1024
-	printf "%s %.1f GiB" "$mem_icon" "$mem_str"
+#if [ "$mem_str" -ge 1024 ]
+#then
+	#let mem_str=$(expr $mem_str / 1024)
+	#printf "%s %.2f GiB" "$mem_icon" "$(($mem_str / 1024))"
 	
-else
-	printf "%s %.0f MiB" "$mem_icon" "$mem_str"
-fi
+#else
+	#printf "%s %.0f MiB" "$mem_icon" "$mem_str"
+#fi
