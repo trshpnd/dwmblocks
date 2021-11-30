cce7fdbc351ae37afc81fcba72518a0f599a6b63
trshpnd <brcts990@gmail.com>
2021-11-30 00:03:34 -0300

    added gitignore, updated blocks.def

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	//{"/\x02", 	"sh /home/trshpnd/dwmblocks/scripts/network.sh", 	10, 	0},
+	//{"\x05  ",	"sh /home/trshpnd/dwmblocks/scripts/volume.sh", 	-1, 	10},
+	{"/\x02", 	"sh /home/trshpnd/Programas/dwmblocks/scripts/weather.sh", 		3600, 	0},
+	{"\x07", 	"sh /home/trshpnd/Programas/dwmblocks/scripts/date.sh", 			10, 	0},
+	{"\x05",	"sh /home/trshpnd/Programas/dwmblocks/scripts/time.sh",			10, 	0},
+	{"\x01",    "sh /home/trshpnd/Programas/dwmblocks/scripts/battery.sh",        60,     0},
 };
-
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "/";
 static unsigned int delimLen = 5;
